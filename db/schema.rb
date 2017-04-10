cd85172135f2aba6091dfbfecf048f525f5eb118
Ainaa Azman <ainaasakinah@gmail.com>
2017-04-10 18:45:39 +0900

    Make models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410084824) do
+ActiveRecord::Schema.define(version: 20170410092914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date     "date"
+    t.integer  "member"
+    t.string   "status"
+    t.integer  "studio_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["studio_id"], name: "index_bookings_on_studio_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "studios", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.integer  "price"
+    t.string   "photo"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +54,6 @@ ActiveRecord::Schema.define(version: 20170410084824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "studios"
+  add_foreign_key "bookings", "users"
 end
