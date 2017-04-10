79f5b70f0c6e76e982e6305c79f71079bb02498f
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-10 19:06:24 +0900

    commented some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages', to: 'pages#home'
-  post 'studios', to: 'studios/create'
-  get 'users', to: 'users/:id'
-  post 'bookings', to: 'bookings/show'
-  get 'studios', to: 'studios/new'
-  get 'studios', to: 'studios/?'
+  root to: 'pages#home'
+  # post 'studios', to: 'studios/create'
+  # get 'users', to: 'users/:id'
+  # post 'bookings', to: 'bookings/show'
+  # get 'studios', to: 'studios/new'
+  # get 'studios', to: 'studios/?'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
