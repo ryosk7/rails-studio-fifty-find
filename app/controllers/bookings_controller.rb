185c7dff29956b1c26faef9fa666ad15d9fb5b85
Ainaa Azman <ainaasakinah@gmail.com>
2017-04-11 17:17:40 +0900

    Added pending status

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_studio, only: [:new, :create]
-  before_action :set_user, only: [:index, :create]
+  before_action :set_user, only: [:index, :create, :show, :new]
 
   def index
     @bookings = Booking.where(user: @user)
@@ -8,12 +8,16 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    if @user.nil?
+      redirect_to new_user_session_path
+    end
   end
 
   def create
     @booking = Booking.new(booking_param)
     @booking.studio = @studio
     @booking.user = @user
+    @booking.status = "Pending"
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -23,6 +27,7 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    @bookings = Booking.where(user: @user)
   end
 
   private
