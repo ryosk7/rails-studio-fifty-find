fd819fd22ae4d242cdc7cf68cfcd76f8cda4fb19
Ainaa Azman <ainaasakinah@gmail.com>
2017-04-12 19:35:54 +0900

    Functional search button cross fingers

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -14,6 +14,14 @@ class Studio < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
 
+  scope :available_for, ->(date) do
+    reserved_studio_ids = Booking.on(date).pluck('DISTINCT studio_id')
+    if reserved_studio_ids.empty?
+      all
+    else
+      where('id NOT IN (?)', reserved_studio_ids)
+    end
+  end
   # Find all available studios through their bookings
   # scope :available_for, ->(date) { joins(:bookings).group("studios.id, bookings.date") }
 end
