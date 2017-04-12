8a29adc01723aa5ea85e30d864264d2cd6f99b06
Douglas Berkley <douglasmberkley@gmail.com>
2017-04-12 19:06:55 +0900

    offers controller finished. needs views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings, dependent: :destroy
+  has_many :offers, through: :studios, source: :bookings
   has_many :studios
   validates :first_name, presence: true
   validates :last_name, presence: true
