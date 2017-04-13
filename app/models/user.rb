7ad5148c955f46bc4149a30c4186732c33c3b1f1
Douglas Berkley <douglasmberkley@gmail.com>
2017-04-13 10:47:22 +0900

    added the mails on the signup feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_create :send_welcome_email
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings, dependent: :destroy
@@ -10,4 +12,9 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :email, presence: true
+
+  private
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 end
